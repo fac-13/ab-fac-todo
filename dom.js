65cfbc4fe584887bb4fb81847981596fde2fe7d0
Jennah <contactjennah@gmail.com>
2018-03-08 14:19:29 +0000

    Implement checkbox functionality to mark todos

--- dom.js
+++ dom.js
@@ -2,7 +2,7 @@
 // part 2 linking it all together
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
-(function() {
+(function () {
   // This is the dom node where we will keep our todo
   var container = document.getElementById('todo-container');
   var addTodoForm = document.getElementById('add-todo');
@@ -14,25 +14,38 @@
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
-  var createTodoNode = function(todo) {
+  var createTodoNode = function (todo) {
     var todoNode = document.createElement('li');
+
     // you will need to use addEventListener
     var displayDescription = todo.description;
-    // add span holding description
+
+    // add a checkbox
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    todoNode.appendChild(checkbox);
+
+    // add span holding description 
     var text = document.createTextNode(displayDescription);
     todoNode.appendChild(text);
+
     // this adds the delete button
     var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    deleteButtonNode.addEventListener('click', function (event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
     todoNode.appendChild(deleteButtonNode);
 
-    // add markTodo button
-    
 
-
+    // add markTodo
+    checkbox.addEventListener('click', function() {
+      console.log(state);
+      console.log(todo.id);
+      var newState = todoFunctions.markTodo(state, todo.id);
+      update(newState);
+      console.log(state);
+    });
 
     // add classes for css
 
@@ -41,29 +54,29 @@
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
+    addTodoForm.addEventListener('submit', function (event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
       // what does event.preventDefault do?
       // what is inside event.target?
       event.preventDefault();
       var description = event.target[0].value;
-      var newState = todoFunctions.addTodo(state, {description: description});
+      var newState = todoFunctions.addTodo(state, { description: description });
       update(newState);
       addTodoForm.reset(); // clears the description field in the form on submission.
     });
   }
 
   // you should not need to change this function
-  var update = function(newState) {
+  var update = function (newState) {
     state = newState;
     renderState(state);
   };
 
   // you do not need to change this function
-  var renderState = function(state) {
+  var renderState = function (state) {
     var todoListNode = document.createElement('ul');
 
-    state.forEach(function(todo) {
+    state.forEach(function (todo) {
       todoListNode.appendChild(createTodoNode(todo));
     });
 
